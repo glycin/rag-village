e5a34a53b6fadb82468c2d879e377a14eaacf314
glycin <alex.chatzizacharias@gmail.com>
2024-07-30 00:34:53 +0200

    created spring boot project

---
+++ src/main/kotlin/com/glycin/ragvillage/RagVillageApplication.kt
@@ -0,0 +1,11 @@
+package com.glycin.ragvillage
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RagVillageApplication
+
+fun main(args: Array<String>) {
+	runApplication<RagVillageApplication>(*args)
+}
---
+++ src/test/kotlin/com/glycin/ragvillage/RagVillageApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.glycin.ragvillage
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RagVillageApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
